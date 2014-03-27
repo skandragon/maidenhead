16b04fc1fba71f378abcf7330868f1327cbecbf8
Michael Graff <explorer@flame.org>
2014-03-27 01:06:10 -0500

    change internal code to be simplier to read

--- lib/maidenhead.rb
+++ lib/maidenhead.rb
@@ -2,7 +2,6 @@
 # Easily convert between latitude and longitude coordinates the the Maidenhead
 # Locator System coordinates.
 class Maidenhead
-
   #
   # Verify that the provided Maidenhead locator string is valid.
   #
@@ -35,48 +34,51 @@ class Maidenhead
   # For each grid, an arbitrary but repeatable latitude and longitude
   # is returned.
   def self.to_latlon(location)
+    maidenhead = Maidenhead.new
+    maidenhead.locator = location
+    maidenhead.latlon
+  end
 
-    unless valid_maidenhead?(location)
+  def pad_locator
+    length = @locator.length / 2
+    while (length < 5)
+      if (length % 2) == 1
+        @locator += '55'
+      else
+        @locator += 'LL'
+      end
+      length = @locator.length / 2
+    end
+  end
+
+  def convert_part_to_latlon(counter, divisor)
+    grid_lon = @locator[counter * 2, 1]
+    grid_lat = @locator[counter * 2 + 1, 1]
+
+    @lat += l2n(grid_lat) * 10.0 / divisor
+    @lon += l2n(grid_lon) * 20.0 / divisor
+  end
+
+  def locator=(location)
+    unless Maidenhead.valid_maidenhead?(location)
       raise ArgumentError.new("Location is not a valid Maidenhead Locator System string")
     end
 
-    length = location.length / 2
-    while (length < 5)
-      if (length % 2) == 1
-        location += '55'
-      else
-        location += 'LL'
-      end
-      length = location.length / 2
-    end
+    @locator = location
+    @lat = -90.0
+    @lon = -180.0
 
-    lat = 0
-    lon = 0
+    pad_locator
 
-    lat_multiplier = 10.0
-    lon_multiplier = 20.0
+    convert_part_to_latlon(0, 1)
+    convert_part_to_latlon(1, 10)
+    convert_part_to_latlon(2, 10 * 24)
+    convert_part_to_latlon(3, 10 * 24 * 10)
+    convert_part_to_latlon(4, 10 * 24 * 10 * 24)
+  end
 
-    5.times do |counter|
-      grid_lon = location[counter * 2, 1]
-      grid_lat = location[counter * 2 + 1, 1]
-
-      if counter == 0
-        lat += l2n(grid_lat) * lat_multiplier
-        lon += l2n(grid_lon) * lon_multiplier
-      elsif (counter % 2) == 0
-        lat_multiplier /= 24
-        lon_multiplier /= 24
-        lat += l2n(grid_lat) * lat_multiplier
-        lon += l2n(grid_lon) * lon_multiplier
-      else
-        lat_multiplier /= 10
-        lon_multiplier /= 10
-        lat += grid_lat.to_i * lat_multiplier
-        lon += grid_lon.to_i * lon_multiplier
-      end
-    end
-
-    [ (lat - 90).round(6), (lon - 180).round(6) ]
+  def latlon
+    [ @lat.round(6), @lon.round(6) ]
   end
 
   #
@@ -85,15 +87,48 @@ class Maidenhead
   # -180 and 180.  Precision defaults to 5 blocks, which returns 10 characters.
   # More precise coordinates may work, but accuracy is not guaranteed.
   def self.to_maidenhead(lat, lon, precision = 5)
-    locator = ''
+    maidenhead = Maidenhead.new
+    maidenhead.lat = lat
+    maidenhead.lon = lon
+    maidenhead.precision = precision
+    maidenhead.locator
+  end
 
-    lat = lat.to_f + 90
-    lon = lon.to_f + 180
+  def lat=(pos)
+    @lat = pos.to_f
+  end
+
+  def lat
+    @lat
+  end
+
+  def lon=(pos)
+    @lon = pos.to_f
+  end
+
+  def lon
+    @lon
+  end
+
+  def precision=(value)
+    @precision = value
+  end
+
+  def precision
+    @precision
+  end
+
+  def locator
+    @locator = ''
+
+    lat = @lat + 90.0
+    lon = @lon + 180.0
+    precision = @precision
 
     # Calculate the field
     lat = (lat / 10) + 0.0000001
     lon = (lon / 20) + 0.0000001
-    locator += n2l(lon.floor).upcase + n2l(lat.floor).upcase
+    @locator += n2l(lon.floor).upcase + n2l(lat.floor).upcase
     precision -= 1
 
     # Calculate the remaining values
@@ -101,24 +136,28 @@ class Maidenhead
       if (counter % 2) == 0
         lat = (lat - lat.floor) * 10
         lon = (lon - lon.floor) * 10
-        locator += "#{lon.floor}#{lat.floor}"
+        @locator += "#{lon.floor}#{lat.floor}"
       else
         lat = (lat - lat.floor) * 24
         lon = (lon - lon.floor) * 24
-        locator += n2l(lon.floor) + n2l(lat.floor)
+        @locator += n2l(lon.floor) + n2l(lat.floor)
       end
     end
 
-    locator
+    @locator
   end
 
   private
 
-  def self.l2n(letter)
-    letter.downcase.ord - 97
+  def l2n(letter)
+    if letter =~ /[0-9]+/
+      letter.to_i
+    else
+      letter.downcase.ord - 97
+    end
   end
 
-  def self.n2l(number)
+  def n2l(number)
     (number + 97).chr
   end
 end
