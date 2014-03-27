eefad427caaae92390c92f95e2526fd21caecbb5
Michael Graff <explorer@flame.org>
2014-03-27 01:25:40 -0500

    simplify code surrounding conversion from lat/lon to locator

--- lib/maidenhead.rb
+++ lib/maidenhead.rb
@@ -33,11 +33,106 @@ class Maidenhead
   #
   # For each grid, an arbitrary but repeatable latitude and longitude
   # is returned.
+  #
   def self.to_latlon(location)
     maidenhead = Maidenhead.new
     maidenhead.locator = location
-    maidenhead.latlon
+    [ maidenhead.lat, maidenhead.lon ]
   end
+
+  #
+  # Set the locator string.  It must be a valid string, or an ArgumentError
+  # will be raised.  This also directly computes the latitude and longitude
+  # values for this locator, so they will be valid after caling this method.
+  #
+  def locator=(location)
+    unless Maidenhead.valid_maidenhead?(location)
+      raise ArgumentError.new("Location is not a valid Maidenhead Locator System string")
+    end
+
+    @locator = location
+    @lat = -90.0
+    @lon = -180.0
+
+    pad_locator
+
+    convert_part_to_latlon(0, 1)
+    convert_part_to_latlon(1, 10)
+    convert_part_to_latlon(2, 10 * 24)
+    convert_part_to_latlon(3, 10 * 24 * 10)
+    convert_part_to_latlon(4, 10 * 24 * 10 * 24)
+  end
+
+  #
+  # Convert from latitude and longitude to a Maidenhead locator string.
+  # Latitude should be between -90 and 90, and longitude should be between
+  # -180 and 180.  Precision defaults to 5 blocks, which returns 10 characters.
+  # More precise coordinates may work, but accuracy is not guaranteed.
+  #
+  def self.to_maidenhead(lat, lon, precision = 5)
+    maidenhead = Maidenhead.new
+    maidenhead.lat = lat
+    maidenhead.lon = lon
+    maidenhead.precision = precision
+    maidenhead.locator
+  end
+
+  #
+  # Set the latitude.  Values must be between -90.0 and +90.0 or an
+  # ArgumentError will be raised.
+  #
+  def lat=(pos)
+    pos = pos.to_f
+    if pos < -90.0 or pos > 90.0
+      raise ArgumentError.new("lat must be between -90.0 and +90.0")
+    end
+
+    @lat = pos
+  end
+
+  def lat
+    @lat.round(6)
+  end
+
+  #
+  # Set the latitude.  Values must be between -90.0 and +90.0 or an
+  # ArgumentError will be raised.
+  #
+  def lon=(pos)
+    pos = pos.to_f
+    if pos < -180.0 or pos > 180.0
+      raise ArgumentError.new("lat must be between -180.0 and +180.0")
+    end
+
+    @lon = pos
+  end
+
+  def lon
+    @lon.round(6)
+  end
+
+  def precision=(value)
+    @precision = value
+  end
+
+  def precision
+    @precision
+  end
+
+  def locator
+    @locator = ''
+
+    @lat_tmp = @lat + 90.0
+    @lon_tmp = @lon + 180.0
+    @precision_tmp = @precision
+
+    calculate_field
+    calculate_values
+
+    @locator
+  end
+
+  private
 
   def pad_locator
     length = @locator.length / 2
@@ -59,95 +154,33 @@ class Maidenhead
     @lon += l2n(grid_lon) * 20.0 / divisor
   end
 
-  def locator=(location)
-    unless Maidenhead.valid_maidenhead?(location)
-      raise ArgumentError.new("Location is not a valid Maidenhead Locator System string")
+  def calculate_field
+    @lat_tmp = (@lat_tmp / 10) + 0.0000001
+    @lon_tmp = (@lon_tmp / 20) + 0.0000001
+    @locator += n2l(@lon_tmp.floor).upcase + n2l(@lat_tmp.floor).upcase
+    @precision_tmp -= 1
+  end
+
+  def compute_locator(counter, divisor)
+    @lat_tmp = (@lat_tmp - @lat_tmp.floor) * divisor
+    @lon_tmp = (@lon_tmp - @lon_tmp.floor) * divisor
+
+    if (counter % 2) == 0
+      @locator += "#{@lon_tmp.floor}#{@lat_tmp.floor}"
+    else
+      @locator += n2l(@lon_tmp.floor) + n2l(@lat_tmp.floor)
     end
-
-    @locator = location
-    @lat = -90.0
-    @lon = -180.0
-
-    pad_locator
-
-    convert_part_to_latlon(0, 1)
-    convert_part_to_latlon(1, 10)
-    convert_part_to_latlon(2, 10 * 24)
-    convert_part_to_latlon(3, 10 * 24 * 10)
-    convert_part_to_latlon(4, 10 * 24 * 10 * 24)
   end
 
-  def latlon
-    [ @lat.round(6), @lon.round(6) ]
-  end
-
-  #
-  # Convert from latitude and longitude to a Maidenhead locator string.
-  # Latitude should be between -90 and 90, and longitude should be between
-  # -180 and 180.  Precision defaults to 5 blocks, which returns 10 characters.
-  # More precise coordinates may work, but accuracy is not guaranteed.
-  def self.to_maidenhead(lat, lon, precision = 5)
-    maidenhead = Maidenhead.new
-    maidenhead.lat = lat
-    maidenhead.lon = lon
-    maidenhead.precision = precision
-    maidenhead.locator
-  end
-
-  def lat=(pos)
-    @lat = pos.to_f
-  end
-
-  def lat
-    @lat
-  end
-
-  def lon=(pos)
-    @lon = pos.to_f
-  end
-
-  def lon
-    @lon
-  end
-
-  def precision=(value)
-    @precision = value
-  end
-
-  def precision
-    @precision
-  end
-
-  def locator
-    @locator = ''
-
-    lat = @lat + 90.0
-    lon = @lon + 180.0
-    precision = @precision
-
-    # Calculate the field
-    lat = (lat / 10) + 0.0000001
-    lon = (lon / 20) + 0.0000001
-    @locator += n2l(lon.floor).upcase + n2l(lat.floor).upcase
-    precision -= 1
-
-    # Calculate the remaining values
-    precision.times do |counter|
+  def calculate_values
+    @precision_tmp.times do |counter|
       if (counter % 2) == 0
-        lat = (lat - lat.floor) * 10
-        lon = (lon - lon.floor) * 10
-        @locator += "#{lon.floor}#{lat.floor}"
+        compute_locator(counter, 10)
       else
-        lat = (lat - lat.floor) * 24
-        lon = (lon - lon.floor) * 24
-        @locator += n2l(lon.floor) + n2l(lat.floor)
+        compute_locator(counter, 24)
       end
     end
-
-    @locator
   end
-
-  private
 
   def l2n(letter)
     if letter =~ /[0-9]+/
